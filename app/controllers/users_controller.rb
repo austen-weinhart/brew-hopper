e166d354717a0cc368e052be968f2702ce3e5325
Curtis Ott <ott_curtis@yahoo.com>
2015-05-05 20:09:56 -0700

    reverted to original user setup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,5 @@
 class UsersController < ApplicationController
   before_action :require_login, except: [:new, :create]
-  before_save :format_user_input
-
-  attr_reader :password
-
-  def password=(unencrypted_password)
-    unless unencrypted_password.empty?
-      @password = unencrypted_password
-      self.password_digest = BCrypt::Password.create(unencrypted_password)
-    end
-  end
-
-  def authenticate(unencrypted_password)
-    if BCrypt::Password.new(self.password_digest) == unencrypted_password
-      return self
-    else
-      return false
-    end
-  end
-
-  def format_user_input
-    self.name = self.name.titleize
-    self.email = self.email.downcase
-  end
   
   def index
     @users = User.all
@@ -49,7 +26,7 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-
+    
     if @user.update_attributes(user_params)
       redirect_to edit_user_path(current_user)
     else
@@ -57,16 +34,8 @@ class UsersController < ApplicationController
     end
   end
 
-  #validations
-  validates :name, presence: true
-  validates :email, presence: true, uniqueness: {case_sensitive: false}, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
-
-  has_secure_password
-  validates :password, presence: true, confirmation: true, length: {in: 6..20}, allow_blank: true
-
-
   private
-
+  
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
