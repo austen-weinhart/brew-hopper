1a60f1fcc3623541654ff93e42130b92c654b817
Austen Weinhart <austen.weinhart@gmail.com>
2015-05-08 15:17:47 -0700

    committing before pulling

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 20150508212055) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "beers", force: :cascade do |t|
     t.string   "name"
     t.string   "image_url"
-    t.string   "style"
-    t.string   "brewers"
     t.float    "abv"
     t.string   "availability"
     t.text     "description"
