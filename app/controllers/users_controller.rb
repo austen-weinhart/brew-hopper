026f584ff6e30263e4e726c17f3cd6e2ab69ab46
Curtis Ott <ott_curtis@yahoo.com>
2015-05-14 14:25:56 -0700

    added login/sign up for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  # before_action :require_login, except: [:new, :create]
+  before_action :require_login, except: [:new, :create]
   
   def index
     @users = User.all
@@ -21,17 +21,17 @@ class UsersController < ApplicationController
   end
 
   def edit
-    # authorized?
+    authorized?
   end
 
   def update
     @user = User.find(params[:id])
     
-    # if @user.update_attributes(user_params)
-    #   redirect_to edit_user_path(current_user)
-    # else
-    #   render :edit
-    # end
+    if @user.update_attributes(user_params)
+      redirect_to edit_user_path(current_user)
+    else
+      render :edit
+    end
   end
 
   private
