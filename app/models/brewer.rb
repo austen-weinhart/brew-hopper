f0e97ad872b1aab659b5ee210e244a903de3cfd6
Daniel Dowling <ddowling30@gmail.com>
2015-05-14 18:41:59 -0700

    added routes to all views to 'go back'

--- app/models/brewer.rb
+++ app/models/brewer.rb
@@ -2,15 +2,10 @@ class Brewer < ActiveRecord::Base
   has_many :beers
 
   validates :brewery, presence: true
-<<<<<<< HEAD
   validates :website, presence: true
   validates :address, presence: true
-  # validates :state, presence: true
-=======
-  validates :address, presence: true
->>>>>>> master
   validates :zip_code, presence: true
-  validates :website, presence: true
+ 
 
 
 end
